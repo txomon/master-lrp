61d8bc9b74fe3278ac7209a6846d59d1babb47f7
Javier Domingo Cansino <javierdo1@gmail.com>
2015-02-28 20:17:28 +0100

    First part of the exercise

---
+++ operadores-aritmeticos/multiplicador.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    18:02:00 02/28/2015 
+-- Design Name: 
+-- Module Name:    multiplicador - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity multiplicador is
+    Port ( op_a : in  STD_LOGIC_VECTOR (15 downto 0);
+           op_b : in  STD_LOGIC_VECTOR (15 downto 0);
+           res : out  STD_LOGIC_VECTOR (31 downto 0));
+end multiplicador;
+
+architecture Behavioral of multiplicador is
+
+begin
+res <= std_logic_vector(unsigned(op_a) * unsigned(op_b));
+
+end Behavioral;
+
